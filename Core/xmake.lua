9f508d1e5fff6915cc0e22d8b0dfc23ad0d5d4f4
TECH-CE-高云天 <yuntian.gao@booming.com>
2024-03-04 18:21:22 +0800

    init

--- Core/xmake.lua
+++
@@ -1,7 +0,0 @@
-target("Core")
-    set_kind("binary")
-
-    add_headerfiles("**.h")
-    add_files("**.cpp")
-    add_includedirs("include", "src", {public = true})
-target_end()
--- gen_win.bat
+++ gen_win.bat
@@ -1,1 +1,3 @@
+git submodule --init
+git submodule update --recursive
 cmake -S . -B Build -DSHERPHY_DEBUG=1
